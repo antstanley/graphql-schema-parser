e8d132309f2876dc2626b4e15b9aa75b8c56588e
Ant Stanley <antstanley@users.noreply.github.com>
2018-07-20 15:40:16 +0100

    add node_modules and rollup config

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
+
+export default {
+  input: 'src/index.mjs',
+  output: {
+    file: 'dist/index.mjs',
+    format: 'esm'
+  },
+  plugins: [
+    resolve({
+      module: false,
+      main: true,
+      extensions: ['.mjs', '.js', '.json']
+    }),
+    json()
+  ]
+}
